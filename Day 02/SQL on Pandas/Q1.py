a7f4c0e4e02cba365bf4e01adea87e4c2858f2c7
Atharv-Kondekar <atharvkondekar96@gmail.com>
2025-12-13 09:14:15 +0530

    SQL on PANDAS

--- Day 02/SQL on Pandas/Q1.py
+++ Day 02/SQL on Pandas/Q1.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import pandasql as ps
 
+emp_data = pd.read_csv("emp_hdr.csv")
+
+query = """SELECT empno,ename 
+FROM emp_hdr 
+WHERE sal>=1000
+"""
+result = ps.sqldf(query , {"emp_hdr":emp_data} )
+
+print(result)
