f9984e4e81e3c9c267ddb2d71e7bd9949a7e41c2
Atharv-Kondekar <atharvkondekar96@gmail.com>
2025-12-15 11:53:26 +0530

    Chat histroy model

--- Day 02/Streamlit/Chat history/main.py
+++ Day 02/Streamlit/Chat history/main.py
@@ -1,1 +1,15 @@
+import streamlit as st
+st.title("Chat model ")
 
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+message = st.chat_input("Say something.....")
+if message:
+    st.session_state.messages.append(message)
+
+if st.session_state.messages : 
+    st.subheader("Chat History")
+
+    for msg in st.session_state.messages:
+        st.write(msg)
