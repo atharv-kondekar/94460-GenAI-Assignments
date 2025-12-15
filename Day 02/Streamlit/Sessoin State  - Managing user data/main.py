f91d203f0c671172162fa4c6cba5e5068aac9a3b
Atharv-Kondekar <atharvkondekar96@gmail.com>
2025-12-15 11:39:28 +0530

    Session State

--- Day 02/Streamlit/Sessoin State  - Managing user data/main.py
+++ Day 02/Streamlit/Sessoin State  - Managing user data/main.py
@@ -1,1 +1,16 @@
+import streamlit as st
+if 'counter' not in st.session_state:
+    st.session_state.counter = 0
 
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("Increment"):
+        st.session_state.counter += 1
+        st.toast("Counter incremented")
+        
+
+with col2:
+    if st.button("Reset"):
+        st.session_state.counter = 0
+        st.write(f"{st.session_state.counter}")
