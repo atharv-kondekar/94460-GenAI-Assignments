219d3f640d39ff08d7784ea6dad4d25b1d3af243
Atharv-Kondekar <atharvkondekar96@gmail.com>
2025-12-18 10:27:15 +0530

    Ass 05 Q1,Q2

--- Assignment 05/Q1/scrap_data.py
+++ Assignment 05/Q1/scrap_data.py
@@ -1,1 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
 
+options = Options()
+options.add_argument("--headless")
+
+browser = webdriver.Chrome(options=options)
+wait = WebDriverWait(browser, 15)
+
+browser.get("https://www.sunbeaminfo.com/")
+browser.implicitly_wait(5)
+
+# scroll
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(3)
+
+# find toggle (NOT clickable, just present)
+plus_button = wait.until(
+    EC.presence_of_element_located((By.XPATH, "//*[@id='collapseSix']/preceding-sibling::a"))
+)
+
+# click using JS (bypasses Selenium click issues)
+browser.execute_script("arguments[0].click();", plus_button)
+time.sleep(2)
+
+# scrape internship + batch info
+items = browser.find_elements(By.XPATH, "//*[@id='collapseSix']//li")
+
+print("Internship & Batch Info:\n")
+for i in items:
+    if i.text.strip():
+        print(i.text)
+
+browser.quit()
--- Assignment 05/Q2/main.py
+++ Assignment 05/Q2/main.py
@@ -1,1 +1,84 @@
 ## 2. ⁠Connect to Groq and Gemini AI using REST api. Send same prompt and compare results. Also compare the speed.
+import time
+import requests
+import os
+import dotenv
+import json
+
+dotenv.load_dotenv()
+
+#Groq
+api= os.getenv("groq_api")
+url = "https://api.groq.com/openai/v1/chat/completions"
+
+headers = {
+    "Authorization" : f"Bearer {api}",
+    "content-type" : "application/json"
+}
+
+# Gemini 
+gem = os.getenv("gemini_ai")
+gem_url=(  "https://generativelanguage.googleapis.com/v1/"
+    "models/gemini-pro:generateContent"
+    f"?key={gem}")
+
+header = {
+    "content-type" : "application/json"
+}
+
+
+print(" --------------- Using th Groq ---------------- ")
+while True : 
+    user = input("Ask Anything : ")
+
+    if user == "exit" :
+        break
+
+    req_data= {
+        "model" : "llama-3.3-70b-versatile",
+        "messages":[
+            {
+                "role":"user",
+                "content":user
+            }
+        ]
+    }
+
+    time1=time.perf_counter()
+    response = requests.post(url,headers=headers,data=json.dumps(req_data))
+    time2=time.perf_counter()
+
+    res = response.json()
+
+    print("AI : ",res["choices"][0]["message"]["content"])
+    print(f"Time for the Groq : {time2-time1}")
+
+
+    print("------------using Gemini -----------")
+    req ={
+        "contents": [
+            {
+                "parts": [
+                    { "text": user }
+                ]
+            }
+        ]
+    }
+
+    time3=time.perf_counter()
+    result=requests.post(gem_url,headers=header,data=json.dumps(req))
+    time4=time.perf_counter()
+
+    re=result.json()
+
+   # print("AI :", re["candidates"][0]["content"]["parts"][0]["text"])
+    print(re)
+    print(time3)
+    print(time4)
+    print(f"time for Gemini ={time4-time3} ")
+
+
+
+
+
+
