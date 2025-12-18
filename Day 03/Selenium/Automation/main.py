3300994f82bf13acced9697fc02c532a37d5cd57
Atharv-Kondekar <atharvkondekar96@gmail.com>
2025-12-18 19:38:55 +0530

    Automation

--- Day 03/Selenium/Automation/main.py
+++ Day 03/Selenium/Automation/main.py
@@ -1,1 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
+driver = webdriver.Chrome()
+driver.get("https://duckduckgo.com")
+time.sleep(0.5)
+print("Title  : ",driver.title)
+
+#driver.implicitly_wait(5)
+
+search_box = driver.find_element(By.NAME , "q")
+
+search_box.clear()
+search_box.send_keys("Dkte Ichalkaranji")
+time.sleep(0.5)
+search_box.send_keys(Keys.RETURN)
+time.sleep(5)
+print("Titile : ",driver.title)
+
+driver.quit()
