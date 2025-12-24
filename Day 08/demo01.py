9f2892ef84c12d5aada45f232bf70718c5bfaa02
Atharv-Kondekar <atharvkondekar96@gmail.com>
2025-12-24 08:47:37 +0530

    Day 08

--- Day 08/demo01.py
+++ Day 08/demo01.py
@@ -1,1 +1,27 @@
+from langchain.embeddings import init_embeddings
+import numpy as np 
 
+llm = init_embeddings(
+    model="text-embedding-all-minilm-l6-v2-embedding",
+    provider="openai",
+    base_url="http://127.0.0.1:1234/v1",
+    api_key="not needed",
+    check_embedding_ctx_length=False
+)
+
+def cosine_sim(a,b):
+    return np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b) )
+
+sentences =[
+    "I like Artificial Intelligence",
+    "Generative AI is magnificant",
+    "World is amazing"
+]
+
+embeddings = llm.embed_documents(sentences)
+
+for vtr in embeddings:
+    print(f"Length {len(vtr)} --> {embeddings[:4]} ")
+
+print("Senetnce 1 vs Sentence 2 : ",cosine_sim(embeddings[0] , embeddings [1]))
+print("Senetnce 1 vs Sentence 3 : ",cosine_sim(embeddings[0] , embeddings [2]))
---
+++ Day 08/demo03.py
@@ -0,0 +1,38 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.embeddings import init_embeddings
+
+llm = init_embeddings(
+    model="text-embedding-all-minilm-l6-v2-embedding",
+    provider="openai",
+    base_url="http://127.0.0.1:1234/v1",
+    api_key="not needed",
+    check_embedding_ctx_length=False
+)
+
+def load_pdf(pdf_path):
+    loader = PyPDFLoader(pdf_path)
+    docs = loader.load() # returns the list of the documents
+
+    content = ""
+
+    for page in docs :
+        content = content + page.page_content
+    
+    metadata = {
+        "source":pdf_path,
+        "page_count":len(docs)
+    }
+
+    return content , metadata
+
+
+path = "D:/Intrenship/94460-GenAI-Assignments/Day%2008//resume-003.pdf"
+
+resume_text ,  resume_info = load_pdf(path)
+print(resume_info)
+print(resume_text)
+
+
+resume_embeddings = llm.embed_documents([resume_text])
+for vect in resume_embeddings :
+    print(f"Len :{len(vect)} --> {resume_embeddings[:4]} ")
