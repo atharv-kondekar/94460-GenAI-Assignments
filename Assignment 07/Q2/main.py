be1d22e5ad8f7f4f9eb303755e54f5980c5fe8f3
Atharv-Kondekar <atharvkondekar96@gmail.com>
2025-12-26 21:31:36 +0530

    Ass7 Q2

--- Assignment 07/Q2/main.py
+++ Assignment 07/Q2/main.py
@@ -1,1 +1,74 @@
+# Create a Streamlit application that takes a city name as input from the user.
+# Fetch the current weather using a Weather API and use an LLM to explain the weather conditions in simple English.
+import streamlit as st 
+import os 
+import dotenv
+from langchain.chat_models import init_chat_model
+import requests
 
+#st.title("Weather Finder")
+
+dotenv.load_dotenv()
+
+llm = init_chat_model(
+    model="openai/gpt-oss-120b",
+    model_provider="openai",
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.getenv("groq_api")
+)
+
+weather_api = os.getenv("open_weather")
+System_message =f""" You interpret live weather data and explain it in simple, clear English.
+Rules:
+- Do not invent data.
+- No exaggeration or dramatic language.
+- 3 to 5 sentences max.
+- Make the explanation practical (umbrella, clothing, outdoor suitability).
+- If data is missing or invalid, say so directly.
+"""
+
+while True:
+    user = input("\n Enter the City name : ")
+
+    if user.lower() == "exit":
+        break
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={user}&appid={weather_api}&units=metric"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Error : City not found ")
+        continue
+
+    data =response.json()
+
+    temp=data['main']['temp']
+    desc=data['weather'][0]['description']
+    humidity=data['main']['humidity']
+    wind = data['wind']['speed']
+
+    llm_input= f"""
+    City :{user}
+    Tenpeature : {temp}°C
+    Condition:{desc}
+    Humidity:{humidity}%
+    Wind speed:{wind}m/s
+    """
+
+    result = llm.stream(
+        [
+            {
+                "role":"system" ,
+                "content":System_message
+            },
+
+            {
+                "role":"user",
+                "content":llm_input
+            }
+        ]
+    )
+
+    print(f"------The Weather Condition in {user}--------")
+    for chunk in result:
+        print(chunk.content,end="")
